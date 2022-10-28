418e4a4657b92194353fa637f82c337fa16d999d
Ansen123 <ansenfp@gmail.com>
2022-10-28 12:12:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { EmployeeloginComponent } from './employeelogin/employeelogin.component';
+import { SecurityloginComponent } from './securitylogin/securitylogin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminDashbordComponent } from './admin-dashbord/admin-dashbord.component';
+import { FormsModule } from '@angular/forms';
 
+const appRoutes:Routes=[
+  {
+    path:"",component:AdminloginComponent
+  },
+  {
+    path:"emp",component:EmployeeloginComponent
+  },
+  {
+    path:"sec",component:SecurityloginComponent
+  },
+  {
+    path:"adminDashbord",component:AdminDashbordComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AdminloginComponent,
+    EmployeeloginComponent,
+    SecurityloginComponent,
+    AdminDashbordComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
